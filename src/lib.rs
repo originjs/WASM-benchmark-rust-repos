29a6ece71da707c609d5bfa2d20da90a4e9fa276
chenshuming <csmdev@hotmail.com>
2021-12-13 16:54:24 +0800

    multiply_int_vec

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern "C" {}
 
 #[wasm_bindgen]
-pub fn hello() {}
+pub fn multiply_int_vec(src1: &[i32], src2: &[i32], res: &mut [i32], n: usize) {
+    for i in 0..n {
+        res[i] = src1[i] * src2[i];
+    }
+}
--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,17 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use rust_wasm_benchmark_code::multiply_int_vec;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    assert_eq!(1 + 1, 2);
+    let data_size = 3;
+    let src1 = [1, 3, 5];
+    let src2 = [2, 4, 6];
+    let mut res = [0, 0, 0];
+    multiply_int_vec(&src1, &src2, &mut res, data_size);
+    assert_eq!(res, [2, 12, 30])
 }
