6d7d1153dfe9675be06647718e2b46d1cf8417a7
PeterAlfredLee <peteralfredlee@gmail.com>
2022-01-10 21:22:25 +0800

    add collision detection

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,60 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+extern crate serde_json;
+extern crate wasm_bindgen;
+
 #[wasm_bindgen]
 extern "C" {}
 
-#[wasm_bindgen]
-pub fn multiply_double_vec(src1: &[f64], src2: &[f64], res: &mut [f64], n: usize) {
-    for i in 0..n {
-        res[i] = src1[i] * src2[i];
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Serialize, Deserialize)]
+pub struct Position {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Position {
+    pub fn new(x: f64, y: f64, z: f64) -> Position {
+        Position { x: x, y: y, z: z }
     }
+}
+
+#[wasm_bindgen]
+pub fn collision_detection(js_positions: &JsValue, radiuses: &[f64], res: &mut [u8], n: usize) -> i32 {
+    let positions: Vec<Position> = js_positions.into_serde().unwrap();
+
+    let mut count = 0;
+    for (i, p) in positions.iter().enumerate() {
+        let r = radiuses[i];
+        let mut collision = false;
+
+        for j in (i + 1)..n {
+            let p2 = &positions[j];
+            let r2 = radiuses[j];
+            let dx = p.x - p2.x;
+            let dy = p.y - p2.y;
+            let dz = p.z - p2.z;
+            let mut d = (dx * dx + dy * dy + dz * dz).sqrt();
+
+            if (r > d) {
+                collision = true;
+                count += 1;
+                break;
+            }
+        }
+
+        let index = (i / 8);
+        let pos = 7 - (i % 8);
+        if (!collision) {
+            res[index] &= !(1 << pos);
+        } else {
+            res[index] |= (1 << pos);
+        }
+    }
+
+    count
 }
--- tests/web.rs
+++ tests/web.rs
@@ -1,19 +1,25 @@
 //! Test suite for the Web and headless browsers.
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_test::*;
 
-#![cfg(target_arch = "wasm32")]
+// #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use rust_wasm_benchmark_code::multiply_double_vec;
+use collisionDetectionRust::Position;
+use collisionDetectionRust::collision_detection;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
-    let data_size = 3;
-    let src1 = [1.0, 3.0, 5.0];
-    let src2 = [2.0, 4.0, 6.0];
-    let mut res = [0.0, 0.0, 0.0];
-    multiply_double_vec(&src1, &src2, &mut res, data_size);
-    assert_eq!(res, [2.0, 12.0, 30.0])
+    let p1 = Position::new(1.0, 1.0, 1.0);
+    let p2 = Position::new(2.0, 2.0, 2.0);
+    let v = vec![p1, p2];
+    let test = JsValue::from_serde(&v).unwrap();
+    let radiuses: [f64;2] = [5.0, 5.0];
+    let mut res: [i64;2] = [0, 0];
+    let result = collision_detection(&test, &radiuses, &mut res,2);
+    assert_eq!(result, 1);
 }
